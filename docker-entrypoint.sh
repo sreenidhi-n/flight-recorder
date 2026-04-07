1c9c315571c40ebcd94cef196f5abde5844b9128
sreenidhi nallajerla <sreenidhin2002@gmail.com>
2026-04-07 20:26:20 +0530

    bug fixes due to crashloop

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "$TASS_GITHUB_PRIVATE_KEY" > /tmp/private-key.pem
 chmod 600 /tmp/private-key.pem
+export TASS_GITHUB_PRIVATE_KEY_PATH=/tmp/private-key.pem
 exec tass serve --addr :8080 --db /data/tass.db
--- internal/storage/migrations/002_check_ids.sql
+++
@@ -1,2 +0,0 @@
--- Migration 002: consolidated into 001. No-op kept for migration ordering.
-SELECT 1;
